d4eb9206021783ad9b85ebc24eae7cd02679dfb6
barbod mazlaghani <b.amirmazlaghani@gmail.com>
2021-11-05 14:01:32 +0330

    Navigation View Item Added + Profile Picture

--- app/src/main/java/com/example/cutsomcalendarfinal/MainActivity.java
+++ app/src/main/java/com/example/cutsomcalendarfinal/MainActivity.java
@@ -10,10 +10,12 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.myapplication.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     CustomCalendarView customCalendarView;
     DrawerLayout drawerLayout;
+    NavigationView monthNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +28,11 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+        monthNavigationView = findViewById(R.id.navigationView);
+        monthNavigationView.setItemIconTintList(null);
     }
 }
